7218c43b1287a9d2f76452076a83ef5da32dd3f6
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-06-04 06:56:47 -0700

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/HomePage/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h2 className='text-4xl'>Seagras sri lanaka</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
