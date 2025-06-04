4716e1bb5bcf104a68f815cb6315210f1aaaf112
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-06-04 06:16:47 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2 className='text-4xl'>Seagras sri lanaka</h2>
+    </div>
+  )
+}
+
+export default App
