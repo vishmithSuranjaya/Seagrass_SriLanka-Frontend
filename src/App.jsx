7e1c8b9288efb7607f3d382abdd56546075d68fe
vishmithSuranjaya <suranjaya0327@gmail.com>
2025-06-07 01:36:17 -0700

    conflict with login

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
+        
       </BrowserRouter>
     </div>
   );
